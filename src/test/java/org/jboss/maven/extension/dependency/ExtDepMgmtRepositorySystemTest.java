968bd51b22d54273d7af23ea556828670303a9ef
Alex Szczuczko <aszczucz@redhat.com>
2013-02-01 11:35:17 -0500

    Add dummy JUnit test

---
+++ src/test/java/org/jboss/maven/extension/dependency/ExtDepMgmtRepositorySystemTest.java
@@ -0,0 +1,14 @@
+package org.jboss.maven.extension.dependency;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExtDepMgmtRepositorySystemTest {
+
+    @Test
+    public void testNewLocalRepositoryManager() {
+        fail("Not yet implemented");
+    }
+
+}
