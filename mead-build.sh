4a2178277f26ce23cbdecc09337bff4635c70ce5
John Casey <jcasey@redhat.com>
2013-04-15 18:03:35 -0400

    adding mead-build.sh helper script

---
+++ mead-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+brew maven-build --specfile=git://git.app.eng.bos.redhat.com/maven-dependency-management-extension.git#6036fb7 mead-shared-maven3 git://git.app.eng.bos.redhat.com/maven-dependency-management-extension.git#6036fb7
