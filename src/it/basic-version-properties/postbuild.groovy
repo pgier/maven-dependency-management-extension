e5d00ff3cac518967dd0439d7475830c99169347
Paul Gier <pgier@redhat.com>
2014-02-04 16:08:33 -0600

    [issue #7] Add tests for new version properties

---
+++ src/it/basic-version-properties/postbuild.groovy
@@ -0,0 +1,11 @@
+
+import java.util.Properties
+
+Properties versionProps = new Properties()
+File itDirectory = new File("target/it/basic-version-properties")
+
+new File(itDirectory, "target/classes/versions.properties").withReader { reader -> 
+  versionProps.load( reader )
+}
+assert( "4.1".equals( versionProps.getProperty( "theJunitVersion" ) ) )
+
